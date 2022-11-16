cf716b85d87feecf39b4d03c2f847e7d63a9831d
yangxu351 <xuyang_ustb@163.com>
2022-11-16 09:50:58 -0500

    test with dataseed1

--- test_wdt.sh
+++ test_wdt.sh
@@ -8,7 +8,9 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 python tools/test_net.py --config-
 # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 python tools/test_net.py --config-file configs/da_faster_rcnn/e2e_da_faster_rcnn_R_50_FPN_WDT_voc_sd0.yaml --ckpt /data/users/yang/code/sa_da_faster/output/20221115_2308_R-50-FPN_sd0_Weights/modle_final.pth #  0.3298
 
 # 70000 iterations dataseed1
-# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 python tools/test_net.py --config-file configs/da_faster_rcnn/e2e_da_faster_rcnn_R_50_FPN_WDT_voc_sd1.yaml --ckpt /data/users/yang/code/sa_da_faster/output/20221115_0718_R-50-FPN_sd1_Weights/modle_final.pth # 0.4227
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 python tools/test_net.py --config-file configs/da_faster_rcnn/e2e_da_faster_rcnn_R_50_FPN_WDT_voc_sd1.yaml --ckpt /data/users/yang/code/sa_da_faster/output/20221115_0718_R-50-FPN_sd1_Weights/modle_final.pth # 0.4227 
+# 70000 iterations dataseed1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 python tools/test_net.py --config-file configs/da_faster_rcnn/e2e_da_faster_rcnn_R_50_FPN_WDT_voc_sd1.yaml --ckpt /data/users/yang/code/sa_da_faster/output/20221115_0718_R-50-FPN_sd1_Weights/modle_final.pth # 0.4295
 
 # 70000 iterations dataseed2
 # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 python tools/test_net.py --config-file configs/da_faster_rcnn/e2e_da_faster_rcnn_R_50_FPN_WDT_voc_sd2.yaml --ckpt /data/users/yang/code/sa_da_faster/output/20221115_1220_R-50-FPN_sd2_Weights/modle_final.pth # 0.3541
